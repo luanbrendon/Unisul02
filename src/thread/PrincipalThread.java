bcb169ae1e0bc5b52dedc3b4da07c546347d7149
LUAN BRENDON <102001821+luanbrendon@users.noreply.github.com>
2023-03-21 19:57:05 -0300

    Add files via upload

---
+++ src/thread/PrincipalThread.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thread;
+
+/**
+ *
+ * @author 10722128072
+ */
+public class PrincipalThread {
+    public static void main(String[] args) {
+        System.out.println("Numero de threads:" +
+        Thread.activeCount());
+            System.out.println(Thread.currentThread());
+}
+
+}
